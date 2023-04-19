263ff36ab3c07544b2add87c104d60368e0e7664
KG <KeilGriesLP@gmail.com>
2023-04-18 20:34:21 -0400

    Imported backend function to retrieve data from OpenWeatherAPI. Created conditions to visualize retrieved data with either graphs or images. Minor fixes to pluralize text if necessary. Added error handling for invalid place names.

--- main.py
+++ main.py
@@ -2,18 +2,47 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+# Add title, text input, slider, selectbox, and subheader
 st.title('5-Day Weather Forecast')
-
 place = st.text_input('Place: ')
 days = st.slider('Forecast Days', min_value=1, max_value=5,
                  help='Select the number of forecasted days')
-type  = st.selectbox('Select data to view', ('Temperature', 'Sky conditions'))
+type  = st.selectbox('Select data to view', ('Temperature',
+                                             'Sky conditions'))
 
-st.subheader(f'{option} for the next {days} days in {place}')
+if days < 2:
+    plural = ''
+else:
+    plural = 's'
 
-d, t = get_data(place, days, type)
+st.subheader(f'{type} for the next {days} day{plural} in {place}')
 
-figure = px.line(x=d, y=t, labels={'x': 'Date', 'y': 'Temperature (C)'})
-st.plotly_chart(figure)
+# Get the temperature/sky data
+
+if place: # <-- Indicates to only retrieve data if place string exists
+
+    try:
+        filtered_data = get_data(place, days)
+
+    # VISUALIZE DATA
+
+    # Plot graph only if the type 'Temperature' is requested
+        if type == 'Temperature':
+            temperatures = [dict['main']['temp'] / 10 for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
+            st.plotly_chart(figure)
+
+    # Show images only if the type 'Sky conditions' is  requested
+        if type == 'Sky conditions':
+            images = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png',
+                      'Rain': 'images/rain.png', 'Snow': 'images/snow.png'}
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+
+    # Handle error when user enters an invalid place name
+    except KeyError:
+        st.write('Please enter a valid place name.')
 
 
